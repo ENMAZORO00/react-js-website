f63f7c9f1547492e916b2b647228ebea6176b9fa
Shubham Pal <palshubham8960@gmail.com>
2023-11-08 13:20:04 +0530

    first commit

---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = ()=>{
+    return(
+        <div className="footer">
+            <div className="footerbox">
+                 <div className="compfooter">
+                    <div className="footerimage">
+                    <img src="./image/company.png" alt="company logo"/>
+                    </div>
+                    <h1>Launch Your Website and Portfolio</h1>
+                 </div>
+                 <div>
+                    <h1>100K+ creators trust Graphy to teach online</h1>
+                 </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,104 @@
+const Home = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="image">
+                    <img src="./image/profile.webp" alt="logo"></img>
+                </div>
+                <div className="content">
+                    <div className="desc">
+                        <h1>UpCred.AI</h1>
+                        <h5>Find Best AI Solution Here.!</h5>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="desc2">
+                    <h1>Come With Us Into The Future</h1>
+                    <p1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore dicta alias pariatur shfk jfk skflsf sfjkle fsjefj fhlty  sfos  gseog  se ge g esgge deleniti quidem officiis eligendi ut vero sed eum aliquam, cupiditate eos quas ad veritatis iusto! Eos, molestiae delectus.</p1>
+                    <div className="circlecontainer">
+                        <div className="circletext">
+                            <h1>Start With Just A Click!!!</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="image">
+                    <img src="./image/image1.webp" alt="logo"></img>
+                </div>
+                <div className="content">
+                    <div className="desc1">
+                        <h1>Where Do You See Yourself In Future?</h1>
+                        <p1>Come with Us To Explore The Future With Us. It Would Be a Fascinating Ride to Travel Into The Future. </p1>
+                        <p1>Lets GO!. WE WOULD BUILD THE FUTURE FROM Ground Up..</p1>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="desc2">
+                    <h1>The Best Course Is Here!</h1>
+                    <div className="table">
+                        <div className="table1">
+                            <ul>
+                                <li>Time Efficient</li>
+                                <li>Be The Best </li>
+                                <li>First Come</li>
+                                <li>Good To Best</li>
+                                <li>Cost Saving</li>
+                                <li>Rocket Machine</li>
+                            </ul>
+                        </div>
+                        <div className="table1">
+                            <ul>
+                                <li>Time Efficient</li>
+                                <li>Be The Best</li>
+                                <li>First Come</li>
+                                <li>Good To Best</li>
+                                <li>Cost Saving</li>
+                                <li>Rocket Machine</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="desc2">
+                    <h1>Come With Us Into The Future</h1>
+                    <p1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.I am the best coder in yheg dhh vjj fhhhhfh djj dfhh hfhhv hhhb hhhn bjhjjb jjbj Labore dicta alias pariatur deleniti quidem officiis eligendi ut vero sed eum aliquam, cupiditate eos quas ad veritatis iusto! Eos, molestiae delectus.</p1>
+                    <div className="circlecontainer">
+                        <div className="circletext">
+                            <h1>Buy Now!!!</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="body">
+                    <div className="wrapper">
+                        <div className="static-txt">I'm  :</div>
+                        <ul className="dynamic-txts">
+                            <li><span>YouTuber</span></li>
+                            <li><span>Designer</span></li>
+                            <li><span>Developer</span></li>
+                            <li><span>Freelancer</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="image">
+                    <img src="./image/image2.jpg" alt="logo"></img>
+                </div>
+                <div className="content">
+                    <div className="desc">
+                        <h1>"Rachana Rathor"</h1>
+                        <h5>The Best Book Seller In India!</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
